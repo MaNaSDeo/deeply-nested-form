ad6a43a2735f32ebc178c024db717a3fe128864d
manasdeo <rmnslogin@gmail.com>
2024-08-28 23:12:21 +0530

    provider and context

--- components/forms.tsx
+++ components/forms.tsx
@@ -1,7 +1,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import exp from "constants";
-import { useForm } from "react-hook-form";
+import { FormProvider, useForm, useFormContext } from "react-hook-form";
 import { z } from "zod";
+import { type ReactNode } from "react";
 
 /**
  * Schemas
@@ -31,3 +31,20 @@ export const useFormCreateCourse = () =>
   useForm<FormTypeCreateCourse>({
     resolver: zodResolver(schemaCreateCourse),
   });
+
+/**
+ * Context
+ */
+
+export const FormProviderCreateCourse = ({
+  children,
+}: {
+  children: ReactNode;
+}) => {
+  const methods = useFormCreateCourse();
+  return <FormProvider {...methods}>{children}</FormProvider>;
+};
+
+export const useFormContextCreateCourse = () => {
+  useFormContext<FormTypeCreateCourse>();
+};
