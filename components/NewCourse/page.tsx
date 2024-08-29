d0d0ea06ad54061b36de6305ed0dc4bca2ee3ae3
manasdeo <rmnslogin@gmail.com>
2024-08-29 15:38:17 +0530

    handle submit

--- components/NewCourse/page.tsx
+++ components/NewCourse/page.tsx
@@ -80,9 +80,12 @@ const ManageChapters = () => {
       {/* Loop through chapter fields */}
       {fields.map((chapter, chapterIndex) => {
         return (
-          <div className="p-6 bg-gray-200 shadow-lg rounded-lg space-y-3">
+          <div
+            className="p-6 bg-gray-200 shadow-lg rounded-lg space-y-3"
+            key={chapter.id}
+          >
             <div className="flex justify-between">
-              <div className="text-lg mb-2 font-semibold">Chapters</div>{' '}
+              <div className="text-lg mb-2 font-semibold">Chapters</div>
               <button
                 type="button"
                 className="text-red-400 text-xs underline underline-offset-4"
@@ -107,9 +110,10 @@ const ManageChapters = () => {
                 }
               </div>
             </label>
-            <ManageNotes chapterIndex={1} />
+            <ManageNotes chapterIndex={chapterIndex} />
             <div className="text-red-600 text-sm mt-1">
               {/* Error: Chapter notes */}
+              {errors.chapters?.[chapterIndex]?.notes?.message}
             </div>
           </div>
         );
@@ -129,36 +133,61 @@ const ManageChapters = () => {
 };
 
 const ManageNotes = ({ chapterIndex }: { chapterIndex: number }) => {
+  const {
+    register,
+    formState: { errors },
+    watch,
+    control,
+  } = useFormContextCreateCourse();
+
+  const { append, remove, fields } = useFieldArray({
+    name: `chapters.${chapterIndex}.notes`,
+    control,
+  });
   return (
     <div className="space-y-4">
       {/* Loop through notes fields  */}
-      <div className="py-4 bg-white p-6 rounded-lg shadow-xl">
-        <div>
-          <div className="flex justify-between">
-            <div className="mb-2 font-semibold">Note</div>
-            <button
-              type="button"
-              className="text-red-400 text-xs underline underline-offset-4"
-            >
-              Remove note
-            </button>
-          </div>
-          <label title={'Title'} className="inline-block">
-            <div className="mb-1">Content</div>
-            <input
-              placeholder="Enter note"
-              className="border-2 border-gray-600  rounded-lg px-2 py-1 bg-transparent"
-              //   Register: Chapter notes content
-            />
-            <div className="text-red-600">
-              {/* Error: Chapter notes content */}
+      {fields.map((note, noteIndex) => {
+        return (
+          <div className="py-4 bg-white p-6 rounded-lg shadow-xl" key={note.id}>
+            <div>
+              <div className="flex justify-between">
+                <div className="mb-2 font-semibold">Note</div>
+                <button
+                  type="button"
+                  className="text-red-400 text-xs underline underline-offset-4"
+                  onClick={() => remove(noteIndex)}
+                >
+                  Remove note
+                </button>
+              </div>
+              <label title={'Title'} className="inline-block">
+                <div className="mb-1">Content</div>
+                <input
+                  placeholder="Enter note"
+                  className="border-2 border-gray-600  rounded-lg px-2 py-1 bg-transparent"
+                  //   Register: Chapter notes content
+                  {...register(
+                    `chapters.${chapterIndex}.notes.${noteIndex}.content`,
+                  )}
+                />
+                <div className="text-red-600">
+                  {/* Error: Chapter notes content */}
+                  {
+                    errors.chapters?.[chapterIndex]?.notes?.[noteIndex]?.content
+                      ?.message
+                  }
+                </div>
+              </label>
             </div>
-          </label>
-        </div>
-      </div>
+          </div>
+        );
+      })}
+
       <button
         type="button"
         className="text-gray-600 text-center w-full underline underline-offset-4 py-2"
+        onClick={() => append({ content: 'Default content!' })}
       >
         add note
       </button>
